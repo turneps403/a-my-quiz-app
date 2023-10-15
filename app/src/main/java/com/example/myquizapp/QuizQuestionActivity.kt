d3bb5c994f73a2ee662871d5b29f1c423f5295e9
turneps403 <turneps403@mail.ru>
2023-10-15 18:00:32 +0300

    - add UI to question activity

--- app/src/main/java/com/example/myquizapp/QuizQuestionActivity.kt
+++ app/src/main/java/com/example/myquizapp/QuizQuestionActivity.kt
@@ -3,6 +3,9 @@ package com.example.myquizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuizQuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +14,19 @@ class QuizQuestionActivity : AppCompatActivity() {
 
         val questionsList = Constants.getQuestions()
         Log.i("Questions size", "${questionsList.size}")
+
+        val curPos = 1
+        val question: Question? = questionsList[curPos - 1]
+
+        findViewById<ProgressBar>(R.id.progressBar).progress = curPos
+        findViewById<TextView>(R.id.tv_progress).text = "$curPos" + "/" + findViewById<ProgressBar>(R.id.progressBar).max
+
+        findViewById<TextView>(R.id.tv_question).text = question!!.question
+        findViewById<ImageView>(R.id.iv_image).setImageResource(question.image)
+
+        findViewById<TextView>(R.id.tv_option_one).text = question!!.optionOne
+        findViewById<TextView>(R.id.tv_option_two).text = question!!.optionTwo
+        findViewById<TextView>(R.id.tv_option_three).text = question!!.optionThree
+        findViewById<TextView>(R.id.tv_option_four).text = question!!.optionFour
     }
 }
